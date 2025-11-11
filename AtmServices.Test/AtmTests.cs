9d76a34aadd56e3b08d3ecc19f880e95025fd981
Owen Johnson <156932008+Oij13@users.noreply.github.com>
2025-11-11 15:19:08 +0000

    Get 100 percent test coverage

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -20,4 +20,25 @@ public class AtmTests
         Assert.True(result);
         Assert.Equal(75, testAtm.getBalance());
     }
+    [Fact]
+    public void Test_Withdraw_Zero()
+    {
+        var result = testAtm.withdraw(101);
+        Assert.False(result);
+        Assert.Equal(100, testAtm.getBalance());
+    }
+    [Fact]
+    public void Test_Deposit()
+    {
+        var result = testAtm.deposit(40);
+        Assert.True(result);
+        Assert.Equal(140, testAtm.getBalance());
+    }
+    [Fact]
+    public void Test_Deposit_Negative()
+    {
+        var result = testAtm.deposit(-1);
+        Assert.False(result);
+        Assert.Equal(100, testAtm.getBalance());
+    }
 }
